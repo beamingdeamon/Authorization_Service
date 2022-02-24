4253d8b90ecb3b73a6d2db49cae9775c0bb7c378
Beaming_Deamon <51133964+beamingdeamon@users.noreply.github.com>
2022-02-24 21:46:38 +0600

    factories
    two factories added

--- database/factories/UserInfoFactory.php
+++ database/factories/UserInfoFactory.php
@@ -2,19 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\UserInfo;
+use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserInfoFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = UserInfo::class;
     public function definition()
     {
         return [
-            //
+            'user_id'=> User::all()->random()->id,
+            'first_name'=> $this->faker->firstName,
+            'last_name'=> $this->faker->lastName,
+            'role_id' => Role::all()->random()->id,
         ];
     }
 }
